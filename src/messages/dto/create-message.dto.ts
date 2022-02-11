ab11cf04c9752c445bb163f28cd6efb99d896e5c
nout <venom12@inbox.ru>
2022-02-11 14:49:09 +0300

    validation name, message regexp, { message: 'Недопустимые символы в строке'

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,14 +1,16 @@
 // import { ApiProperty } from '@nestjs/swagger';
 
-import { IsString, Length } from 'class-validator';
-
+import { IsString, Length, Matches } from 'class-validator';
+const regexp = /^([а-яА-ЯёЁa-zA-Z0-9]){3,30}$/;
 export class CreateMessageDto {
   // @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   @IsString({ message: 'Должно быть строкой' })
   @Length(4, 16, { message: 'Минимальная длина имени 3 символов, а максимальная 30' })
+  @Matches(regexp, { message: 'Недопустимые символы в строке' })
   readonly name: string;
   // @ApiProperty({ example: '123456789', description: 'пароль' })
   @IsString({ message: 'Должно быть строкой' })
   @Length(10, 1000, { message: 'Минимальная длина сообщения 10 символов, а максимальная 1000' })
+  @Matches(regexp, { message: 'Недопустимые символы в строке' })
   readonly message: string;
 }
