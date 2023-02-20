a6fe5b514eddb651ea3f66d320301108f7a8a55e
nout <venom12@inbox.ru>
2023-02-20 13:51:34 +0300

    fix comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { Intro } from './components/Intro';
